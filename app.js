e2542644cd219359bd26a9eb1dc936a7e8e59ba5
SoloRatRacer97 <todd.j.anderson.1997@gmail.com>
2023-02-01 08:32:44 -0800

    Added fake user routes and middleware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Guest = require("./models/guests");
 const guestRoutes = require("./routes/guests");
-const ejsMate = require('ejs-mate');
-
+const ejsMate = require("ejs-mate");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const flash = require("connect-flash");
+const User = require("./models/user");
+const { register } = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/weddingAttendees");
 
@@ -17,15 +23,44 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 // router seems to not like guests.. but it works fine without it??
-app.use('/guests', guestRoutes)
+app.use("/guests", guestRoutes);
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", async (req, res) => {
+  res.render("home");
+});
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({ email: "test@test.com", username: "test" });
+  const newUser = await User.register(user, "Pa$$w0rd");
+  res.send(newUser);
+});
 
 app.get("/guests", async (req, res) => {
   const guests = await Guest.find({});
@@ -33,38 +68,38 @@ app.get("/guests", async (req, res) => {
   console.log(guests);
 });
 
-app.get('/songs', async (req,res) => {
+app.get("/songs", async (req, res) => {
   const guests = await Guest.find({});
-  res.render('guests/songs', { guests });
-})
+  res.render("guests/songs", { guests });
+});
 
 app.get("/guests/new", (req, res) => {
   res.render("guests/new");
 });
 
-app.post('/guests', async (req,res) => {
+app.post("/guests", async (req, res) => {
   const guest = new Guest(req.body.guest);
   await guest.save();
-  res.redirect('/guests/new')
-})
+  res.redirect("/guests/new");
+});
 
-app.get('/guests/edit', async (req, res) => {
+app.get("/guests/edit", async (req, res) => {
   const guests = await Guest.find({});
-  res.render('guests/edit', { guests })
-})
+  res.render("guests/edit", { guests });
+});
 
-app.get ('/guests/:id/guest_edit', async (req, res) => {
+app.get("/guests/:id/guest_edit", async (req, res) => {
   const guest = await Guest.findById(req.params.id);
   const guests = await Guest.find({});
   res.render("guests/guest_edit", { guest, guests });
-})
+});
 
-app.put('/guests/guest_edit/:id', async (req, res) => {
-  const { id } = req.params
+app.put("/guests/guest_edit/:id", async (req, res) => {
+  const { id } = req.params;
   const guest = await Guest.findById(id);
-  await Guest.findByIdAndUpdate(id, { ...req.body.guest })
-  res.redirect('/guests/edit')
-})
+  await Guest.findByIdAndUpdate(id, { ...req.body.guest });
+  res.redirect("/guests/edit");
+});
 
 app.post("/guests", async (req, res) => {
   const guest = new Guest(req.body.guest);
@@ -72,9 +107,9 @@ app.post("/guests", async (req, res) => {
   res.redirect(`/guests`);
 });
 
-app.get("/home", (req,res) => {
+app.get("/home", (req, res) => {
   res.render("home");
-})
+});
 
 app.get("/guests/:id/decision", async (req, res) => {
   const guest = await Guest.findById(req.params.id);
@@ -107,12 +142,12 @@ app.put("/guests/:id", async (req, res) => {
   // console.log(req.body);
 });
 
-app.put('/guests/:id', async (req, res) => {
-  const { id } = req.params
+app.put("/guests/:id", async (req, res) => {
+  const { id } = req.params;
   const guest = await Guest.findById(id);
-  await Guest.findByIdAndUpdate(id, { ...req.body.guest })
-  res.redirect('/guests/edit')
-})
+  await Guest.findByIdAndUpdate(id, { ...req.body.guest });
+  res.redirect("/guests/edit");
+});
 
 app.delete("/guests/:id", async (req, res) => {
   const { id } = req.params;
@@ -120,19 +155,17 @@ app.delete("/guests/:id", async (req, res) => {
   res.redirect("/guests/edit");
 });
 
-app.get('/registry', async (req, res) => {
-  res.render('registry')
-})
+app.get("/registry", async (req, res) => {
+  res.render("registry");
+});
 
-app.get('/bridal_party', async (req, res) => {
-  res.render('bridal_party')
-})
+app.get("/bridal_party", async (req, res) => {
+  res.render("bridal_party");
+});
 
-
-app.get('/photos', async (req, res) => {
-  res.render('photos')
-})
-
+app.get("/photos", async (req, res) => {
+  res.render("photos");
+});
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
